0f30e360dd22fc6ec2b09df5e58ac85f964f532d
Bank_Security <29305998+BankSecurity@users.noreply.github.com>
2022-05-13 11:07:27 +0200

    Update Responsible_Process_IP_Connection.sql

--- Responsible_Process_IP_Connection.sql
+++ Responsible_Process_IP_Connection.sql
@@ -1,1 +1,4 @@
-event_simpleName=ProcessRollup2 OR event_simpleName=ProcessBlocked OR event_simpleName=SyntheticProcessRollup2 [search event_simpleName=NetworkConnectIP4 (RemoteAddressIP4=1.1.1.1) | rename ContextProcessId_decimal AS TargetProcessId_decimal | fields aid TargetProcessId_decimal] | table _time ComputerName UserName FileName CommandLine
+event_simpleName=ProcessRollup2 OR event_simpleName=ProcessBlocked OR event_simpleName=SyntheticProcessRollup2 [search event_simpleName=NetworkConnectIP4 (RemoteAddressIP4=1.1.1.1)] 
+| rename ContextProcessId_decimal AS TargetProcessId_decimal 
+| fields aid TargetProcessId_decimal] 
+| table _time ComputerName UserName FileName CommandLine
