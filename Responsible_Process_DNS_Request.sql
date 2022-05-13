83e96b4e41cde2ab7a732b6cf5c3496c41d9b8b4
Bank_Security <29305998+BankSecurity@users.noreply.github.com>
2022-05-13 11:06:13 +0200

    Update Responsible_Process_DNS_Request.sql

--- Responsible_Process_DNS_Request.sql
+++ Responsible_Process_DNS_Request.sql
@@ -1,4 +1,4 @@
-event_simpleName=ProcessRollup2 OR event_simpleName=SyntheticProcessRollup2 OR event_simpleName=ProcessBlocked [search event_simpleName=DnsRequest (DomainName=google.com) 
+event_simpleName=ProcessRollup2 OR event_simpleName=SyntheticProcessRollup2 OR event_simpleName=ProcessBlocked [search event_simpleName=DnsRequest (DomainName=google.com)] 
 | rename ContextProcessId_decimal AS TargetProcessId_decimal 
 | fields aid TargetProcessId_decimal] 
 | table _time ComputerName UserName FileName CommandLine
